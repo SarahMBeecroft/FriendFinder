ed31a52cb2aa6accb829b046f3517c320a70296d
Sarah B <smblair87@gmail.com>
2019-05-18 12:58:16 -0700

    Added some mock friend data and came up with list of 10 questions

---
+++ app/data/friend.js
@@ -0,0 +1,98 @@
+//  Friend DATA
+// =============================================================
+var friends = [
+  {
+    name: "Leslie Knope",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Leslie_Knope_%28played_by_Amy_Poehler%29.png/240px-Leslie_Knope_%28played_by_Amy_Poehler%29.png",
+    scores: [
+      5,
+      4,
+      2,
+      5,
+      1,
+      5,
+      2,
+      4,
+      3,
+      4
+    ]
+  },
+  {
+    name: "Ron Swanson",
+    photo: "https://parade.com/wp-content/uploads/2013/10/tv-show-best-boss-ron-swanson.jpg",
+    scores: [
+      1,
+      1,
+      3,
+      5,
+      2,
+      1,
+      5,
+      2,
+      1,
+      5
+    ]
+  },
+  {
+    name: "Andy Dwyer",
+    photo: "https://d1np8g368n9o2h.cloudfront.net/af55805c-191d-48fb-858a-b0d1bcf4f608",
+    scores: [
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      5,
+      3,
+      5,
+      4
+    ]
+  },
+  {
+    name: "Jerry Gergich",
+    photo: "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr05/2013/7/27/0/enhanced-buzz-14510-1374900472-14.jpg?downsize=700:*&output-format=auto&output-quality=auto",
+    scores: [
+      4,
+      4,
+      1,
+      4,
+      3,
+      5,
+      2,
+      5,
+      4,
+      3
+    ]
+  },
+  {
+    name: "April Ludgate",
+    photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/e/ed/April-0.png/revision/latest?cb=20180116070637",
+    scores: [
+      1,
+      2,
+      4,
+      3,
+      4,
+      4,
+      2,
+      2,
+      2,
+      5
+    ]
+  }];
+  
+
+  // Questions
+  // 1. When it comes to getting work done, you're more of a team player
+  // 2. After going out with a group of friends, you feel energized
+  // 3. You find yourself quick to anger in sticky situations
+  // 4. You are a motivated individual and always strive for improvement
+  // 5. You have hard time staying focused on the task at hand
+  // 6. Getting involved in your community is a big priority for you
+  // 7. You have a lot of hobbies and push yourself creatively outside of work
+  // 8. You are a great listener and people often come to you for advice
+  // 9. You tend to be more of a creative thinker, rather than a logical one
+  // 10. When it comes to friends, you believe in quality over quantity
+
+  // 1-5, 1 being never, 5 being always
