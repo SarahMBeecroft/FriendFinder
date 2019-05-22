331e7469c792642dd9e4f6c6ae07c9ad51bca4e6
Sarah B <smblair87@gmail.com>
2019-05-21 21:13:03 -0700

    Started working on apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,13 @@
-// var friendsData = require ("../data/friend.js");
+// Pulls data from friends.js
+var friendsArray = require("../data/friends.js");
 
-// module.exports = function (app) {
+module.exports = function (app) {
 
-//   app.get("/api/tables", function (req, res) {
-//     res.json(friendsData);
-//   })
+  app.get("/api/friends", function (req, res) {
+    res.json(friendsArray);
+  });
 
-// }
+  app.post("/api/friends", function (req, res) {
+
+  })
+};
